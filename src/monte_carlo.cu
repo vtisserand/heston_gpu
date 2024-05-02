369fe64572e877d6fa0330453a0124502fd7c1f0
Vivien Tisserand <vivien.tisserand@gmail.com>
2024-05-02 15:56:11 +0200

    fix: new params set

--- src/monte_carlo.cu
+++ src/monte_carlo.cu
@@ -38,11 +38,11 @@ __global__ void MC_Heston(curandState* state,
     for (int i = 0; i < N; i++) {
         Ws = curand_normal2(&localState);
         V += kappa * (theta - V) * dt * dt + sigma * sqrtf(fmaxf(0.0f, V)) * dt * Ws.x;
-        S += r * S * dt + sqrtf(V) * S * dt * (rho * Ws.x + sqrtf(1 - rho * rho) * Ws.y);
+        S += r * S * dt * dt + sqrtf(V) * S * dt * (rho * Ws.x + sqrtf(1 - rho * rho) * Ws.y);
     }
 
-    R1s[threadIdx.x] = expf(-r * dt * dt * N) * fmaxf(0.0f, S - K) / n; // Call price
-    R2s[threadIdx.x] = R1s[threadIdx.x] * R1s[threadIdx.x] * n;
+    R1s[threadIdx.x] = expf(-r * dt * dt * N) * fmaxf(0.0f, S - K); // Call price
+    R2s[threadIdx.x] = R1s[threadIdx.x] * R1s[threadIdx.x];
 
     __syncthreads(); // Block-level synchronisation
 
@@ -58,8 +58,8 @@ __global__ void MC_Heston(curandState* state,
 
     // Update global memory atomically
     if (threadIdx.x == 0) {
-        atomicAdd(sum, R1s[0]);
-        atomicAdd(sum + 1, R2s[0]);
+        atomicAdd(sum, R1s[0] / n);
+        atomicAdd(sum + 1, R2s[0] / n);
     }
 }
 
@@ -70,47 +70,57 @@ int main(void) {
 	int NB = 1024;
 	int n = NB * NTPB;
 	float T = 5.0f;
-	float S0 = 100.0f;
-	float V0 = 0.09f;
-	float K = S0;
-	float sigma = 1.0f;
-	float r = 0.05f;
+	float S0 = 1.0f;
+	float V0 = 0.1f;
+	float K = 1.0;
+	float sigma = 0.2f;
+	float r = 0.0f;
     float kappa = 2.0f;
-    float theta = 0.09f;
+    float theta = 0.1f;
     float rho = -0.3f;
 	int N = 1000;
 	float dt = sqrtf(T/N);
-	float *sum;
-	cudaMallocManaged(&sum, 2*sizeof(float));
-	cudaMemset(sum, 0, 2*sizeof(float));
 
+	// Allow some memory for sum (options payoff at maturity) and random states.
+	float *sum;
+	cudaMallocManaged(&sum, 2*sizeof(float)); // We save payoff and std
+	cudaMemset(sum, 0, 2*sizeof(float));
 
 	curandState* states;
 	cudaMalloc(&states, n*sizeof(curandState));
 	initCurand<<<NB, NTPB>>>(time(0), states);
 
 	float Tim;
-	cudaEvent_t start, stop;			// GPU timer instructions
-	cudaEventCreate(&start);			// GPU timer instructions
-	cudaEventCreate(&stop);				// GPU timer instructions
-	cudaEventRecord(start, 0);			// GPU timer instructions
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start, 0);
 
-	MC_Heston<<<NB, NTPB, 2*NTPB*sizeof(float)>>>(S0, V0, r, kappa, theta, rho, sigma, dt, K, 
-											N, states, sum, n);
+	// Launch one simulation on each thread.
+	MC_Heston<<<NB, NTPB, 2*NTPB*sizeof(float)>>>(states, 
+												  S0, 
+												  V0, 
+												  r, 
+												  kappa, 
+												  theta, 
+												  rho, 
+												  sigma, 
+												  dt, 
+												  K, 
+												  N, 
+												  sum, 
+											 	  n);
 
-	cudaEventRecord(stop, 0);			// GPU timer instructions
-	cudaEventSynchronize(stop);			// GPU timer instructions
-	cudaEventElapsedTime(&Tim,			// GPU timer instructions
-		start, stop);					// GPU timer instructions
-	cudaEventDestroy(start);			// GPU timer instructions
-	cudaEventDestroy(stop);				// GPU timer instructions
-
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&Tim, start, stop);
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
 
 	printf("The estimated price is equal to %f\n", sum[0]);
 	printf("error associated to a confidence interval of 95%% = %f\n",
 		1.96 * sqrt((double)(sum[1] - (sum[0] * sum[0])))/sqrt((double)n));
 	printf("Execution time %f ms\n", Tim);
-
 
 	cudaFree(sum);
 	cudaFree(states);
